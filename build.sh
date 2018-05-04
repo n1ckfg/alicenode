00378ef78218e37df8b62d4b0ee62975a65e7cc2
Graham Wakefield <grrrwaaa@gmail.com>
2018-05-04 15:37:16 -0400

    suppress unused variable warning

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-clang++ -O3 -Wall -std=c++11 -stdlib=libc++ -fexceptions -Iinclude -Iinclude/uv src/alice.cpp lib/osx/libglfw3.a lib/osx/libuv.a -framework Cocoa -framework IOKit -framework CoreFoundation -framework CoreVideo -o alice
+clang++ -O3 -Wall -Wunused-variable -std=c++11 -stdlib=libc++ -fexceptions -Iinclude -Iinclude/uv src/alice.cpp lib/osx/libglfw3.a lib/osx/libuv.a -framework Cocoa -framework IOKit -framework CoreFoundation -framework CoreVideo -o alice
