ca3c65c22acda136c6f1bd2542ce7904b4b4551a
Graham Wakefield <grrrwaaa@gmail.com>
2018-06-04 12:08:07 -0400

    windows building OK

--- cards/cpp2json/build.bat
+++ cards/cpp2json/build.bat
@@ -7,11 +7,14 @@ if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\%VCVARS6
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\%VCVARS64%" call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\%VCVARS64%"
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\%VCVARS64%" call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\%VCVARS64%"
 
+rem compile & link:
+cl /W3 /EHsc /O2 /I include cpp2json.cpp /link /MACHINE:X64 libclang.lib 
 
-
-
-
-
-echo Exit Code is %errorlevel%
+rem if compiled OK, run it:
+if %errorlevel% neq 0 (
+    echo Exit Code is %errorlevel%
+) else (
+    cpp2json.exe
+)
 
 @endlocal
