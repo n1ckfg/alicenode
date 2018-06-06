bebc9eeeb21f696fc786bbd18e1ae84e9805f3d9
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2018-06-06 13:27:10 -0400

    working on being able to minimize the cards

--- cards/client/cm/addons/fullscreen/fullscreen.js
+++ cards/client/cm/addons/fullscreen/fullscreen.js
@@ -21,7 +21,7 @@
   function setFullscreen(cm) {
     var wrap = cm.getWrapperElement();
     cm.state.fullScreenRestore = {scrollTop: window.pageYOffset, scrollLeft: window.pageXOffset,
-                                  width: wrap.style.width, height: wrap.style.height};
+                                  width: wrap.style.width, height: (wrap.style.height - 30)};
     wrap.style.width = "";
     wrap.style.height = "auto";
     wrap.className += " CodeMirror-fullscreen";
