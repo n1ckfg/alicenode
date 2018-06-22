11aa0490026d95d9b8fc014663e96bf6ae3e604e
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2018-06-22 14:58:00 -0400

    added cpp2json to root folder

---
+++ cpp2json/test copy.h
@@ -0,0 +1,30 @@
+
+namespace jim {
+	struct Foo {
+
+	};
+}
+
+/*
+	Fooey foofoo
+*/
+struct Foo {
+
+	int x = 0;
+
+	// methy meth
+	int meth(int y) {
+		return x*y;
+	}
+
+	float meth() {
+		return 0.f;
+	}
+};
+
+// zappy zapzap
+int zap(float f) {
+
+
+	return 99 * f;
+}7
