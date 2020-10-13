3f9c559f93b0acdf2aa57e4d2383e75df57ea748
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2020-10-13 17:52:26 -0400

    really janky rewrite of alicenode editor

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const WS = require('ws')
 const ReconnectingWebSocket = require('reconnecting-webSocket')
-
+const fs = require('fs')
  
 const options = {
     WebSocket: WS, // custom WebSocket constructor
@@ -8,17 +8,56 @@ const options = {
     maxRetries: 10,
 };
 
-rwsUrl = 'ws://192.168.5.17/9090'
+rwsUrl = 'ws://localhost:9090'
 const rws = new ReconnectingWebSocket(rwsUrl,[], options);
-console.log('trying')
+console.log('connecting')
 rws.addEventListener('open', () => {
-    rws.send('hello!');
+    console.log('connected')
 });
 
-rws.addEventListener('message', (msg) => {
+rws.addEventListener('message', (message) => {
+
+    let msg = JSON.parse(message.data)
+    console.log(msg.cmd)
+    switch(msg.cmd){
+
+      case 'fileSave':
+        fs.writeFileSync(msg.filename, msg.contents)        
+      break
+
+      default: console.log(msg)
+    }
     console.log(msg);
 });
 
 rws.addEventListener('error', (error) => {
     console.log(error);
 });
+
+// var http = require("http"),
+// terminal = require("web-terminal");
+
+// var app = http.createServer(function (req, res) {
+//     res.writeHead(200, {"Content-Type": "text/plain"});
+//     // res.end("Hello World\n");
+// });
+
+// app.listen(1337);
+// console.log("Server running at http://127.0.0.1:1337/");
+
+// terminal(app);
+// console.log("Web-terminal accessible at http://127.0.0.1:1337/terminal");
+
+
+// eventually: list all files in folder, push to server and then to web client... 
+// fs.readdir(__dirname, function (err, files) {
+//     //handling error
+//     if (err) {
+//         return console.log('Unable to scan directory: ' + err);
+//     } 
+//     //listing all files using forEach
+//     files.forEach(function (file) {
+//         // Do whatever you want to do with the file
+//         console.log(file); 
+//     });
+// });
--- server.js
+++ server.js
@@ -5,6 +5,36 @@ const wss = new WebSocket.Server({ port:9090 })
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
+    // console.log(typeof message)
+    let msg = JSON.parse(message)
+    console.log(msg.cmd)
+    switch(msg.cmd){
+
+      case 'fileSave':
+        console.log('snared')
+        wss.clients.forEach((client) => {
+          client.send(message)
+        });
+      break
+
+      default: console.log(msg)
+    }
   })
-  ws.send('ho!')
+
+  // todo: get list of connected Pis!, pass back to webclient
 })
+
+var express = require('express')
+var app = express()
+const path = require('path')
+
+var htmlPath = path.join(__dirname);
+
+app.use(express.static(htmlPath));
+app.use(express.static('node_modules/codemirror'));
+
+var server = app.listen(3000, function () {
+    var host = 'localhost';
+    var port = server.address().port;
+    console.log('listening on http://'+host+':'+port+'/');
+});
